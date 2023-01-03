f947b7b5d537dae8f87c25de298b1e23a0172c9a
Imunury <dipblu0808@gmail.com>
2023-01-03 09:59:30 +0900

    230104

--- src/EX05.java
+++ src/EX05.java
@@ -94,3 +94,16 @@ class Solution44 {
 //        return answer;
 //    }
 //}
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        int[] answer = new int[10];
+        for (int i = 0; i < numlist.length; i++) {
+            if (numlist[i] % n == 0) {
+                for (int j = 0; j < i; j++) {
+                    answer[j] = numlist[i];
+                }
+            }
+        }
+        return answer;
+    }
+}
